fcf454eb1796739265579a489cc42f105c72d976
Rohit Tiwari <110346246+tiwaritech18@users.noreply.github.com>
2023-02-21 10:02:04 +0530

    serach github profile!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import logo from './github-logo.webp'
 
 function App() {
+  const [user,setUser]=useState('');
+  const[userData,setUserData]=useState(null);
+  
+  function handleSearch(e){
+    e.preventDefault();
+    fetch(`https://api.github.com/users/${user}`)
+    .then(response => response.json())
+    .then(data => {
+      setUserData(data)
+    })
+  }
+  function handleChange(e){
+    setUser(e.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="github">
+          <img className='logo' src={logo} alt="" />
+          <h1>Github Profile Search</h1>
+        </div>
+        <form>
+          <label><h3>Enter github username:</h3></label>
+          <input type="text" placeholder="search.." value={user} onChange={handleChange} />
+          <button className="search" onClick={handleSearch}>Search</button>
+        </form>
       </header>
+      {userData ? (<div className='card'>
+          <img src={userData.avatar_url}alt="User Avatar" />
+          <h2>{userData.name}</h2>
+          <h3>{userData.bio}</h3>
+          <a href={userData.html_url}>viewprofile</a>
+        </div>) : null}
+        
     </div>
   );
 }
